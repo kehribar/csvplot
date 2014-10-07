38b6f4d04e876da27eac0904c9a41210fae7585a
kehribar <ihsan@kehribar.me>
2014-10-07 15:35:28 +0300

    typo fix

--- csvplot.py
+++ csvplot.py
@@ -3,8 +3,7 @@
 # Basic CSV plotting script
 # Each column is plotted at a seperate figure.
 #------------------------------------------------------------------------------     
-# 
-THE COFFEEWARE LICENSE (Revision 1):
+# THE COFFEEWARE LICENSE (Revision 1):
 # <ihsan@kehribar.me> wrote this file. As long as you retain this notice you
 # can do whatever you want with this stuff. If we meet some day, and you think
 # this stuff is worth it, you can buy me a coffee in return.    
